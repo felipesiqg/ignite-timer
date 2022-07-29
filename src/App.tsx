96da474831614f2e5c7353bd17652399e6e35860
Felipe Siqueira <felipesgomes22@gmail.com>
2022-07-29 16:29:05 -0300

    Application structure: styled components; config theme; type theme; global style; colors and fonts; set up ESLint

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-export function App() {
+import { ThemeProvider } from 'styled-components'
+import { Button } from './components/Button'
 
+import { GlobalStyle } from './components/styles/global'
+import { defaultTheme } from './components/styles/themes/default'
+
+export function App() {
   return (
-    <div className="App">
-      Ignite Timer
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
